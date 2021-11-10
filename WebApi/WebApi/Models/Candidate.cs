9e0ef136ad1a47b23779f12d9ede80bb69b77b70
Akshay Kumar <akshaykrkat@gmail.com>
2021-11-10 14:52:06 +0530

    Implemented the Db Setup using EntityFramework
    Implemented the Db Setup using EntityFramework

---
+++ WebApi/WebApi/Models/Candidate.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApi.Models
+{
+    public class Candidate
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Column(TypeName ="nvarchar(100)")]
+        public string Fullname { get; set; }
+
+        [Column(TypeName = "nvarchar(10)")]
+        public string Mobile { get; set; }
+
+        [Column(TypeName = "nvarchar(100)")]
+        public string Email { get; set; }
+
+        public int Age { get; set; }
+
+        [Column(TypeName = "nvarchar(3)")]
+        public string BloodFroup { get; set; }
+
+        [Column(TypeName = "nvarchar(100)")]
+        public string Address { get; set; }
+    }
+}
